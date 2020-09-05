bfa4b3de7da2671549bfe013bdb325d5d6489a21
Hyreon <joshuaddd@gmail.com>
2020-09-05 02:30:10 -0400

    Claimtop for national

--- src/main/java/botmanager/frostbalance/commands/meta/TopClaimsCommand.kt
+++ src/main/java/botmanager/frostbalance/commands/meta/TopClaimsCommand.kt
@@ -14,24 +14,41 @@ class TopClaimsCommand(bot: Frostbalance?) : FrostbalanceGuildCommand(bot, array
 ), AuthorityLevel.GENERIC, ContextLevel.ANY) {
 
     override fun executeWithGuild(context: GuildMessageContext, vararg params: String) {
-        val scores = context.gameNetwork.players.associateWith { player ->
-            context.gameNetwork.worldMap.loadedTiles.map { tile -> tile.claimData }
-                    .filter { claimData -> claimData.owningPlayer == player }
-                    .map { claimData -> claimData.claimLevel }
-                    .reduce { acc, level -> acc + level }
-        }
-                .entries
-                .sortedByDescending { it.value }
-                .map {
-            "${it.key.name}: ${Influence(it.value)}"
-        }
-        object : ListMenu<String>(bot, context, scores) {
 
-        }.send()
+        val scores: List<String> =
+        if (params.getOrNull(0)?.equals("national") == true) {
+            context.gameNetwork.associatedGuilds
+                    .associateWith { guild ->
+                        context.gameNetwork.worldMap.loadedTiles.map { tile -> tile.claimData }
+                                .filter { claimData -> claimData.owningNation == guild.nation }
+                                .map { claimData -> claimData.claimLevel }
+                                .reduceOrNull { acc, level -> acc + level}
+                                ?: 0.0
+                    }
+                    .entries
+                    .sortedByDescending { it.value }
+                    .map {
+                        "${it.key.name}: ${Influence(it.value)}"
+                    }
+        } else {
+            context.gameNetwork.players.associateWith { player ->
+                context.gameNetwork.worldMap.loadedTiles.map { tile -> tile.claimData }
+                        .filter { claimData -> claimData.owningPlayer == player }
+                        .map { claimData -> claimData.claimLevel }
+                        .reduce { acc, level -> acc + level }
+            }
+                    .entries
+                    .sortedByDescending { it.value }
+                    .map {
+                        "${it.key.name}: ${Influence(it.value)}"
+                    }
+        }
+
+        object : ListMenu<String>(bot, context, scores) {}.send()
     }
 
     override fun info(authorityLevel: AuthorityLevel?, isPublic: Boolean): String? {
         return "**.claimtop** - Get the players with the most leveled claims\n" +
-                "**.claimtop NATION** - Get the nations with the most claims (does not count internal clash)"
+                "**.claimtop national** - Get the nations with the most claims (does not count internal clash)"
     }
 }
